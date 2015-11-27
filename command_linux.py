9a988e46ba686efb5899e25eec49a6032275d4c6
Zirou <z3rou@hotmail.com>
2015-11-27 16:48:51 -0200

    Create command_linux.py

---
+++ command_linux.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import sys
+import os
+
+if len(sys.argv) <= 3:                # arv = ['./comando.py', 'ls', '-la']
+    comando = ' '.join(sys.argv[1:])  # arv[1:] = ['ls', '-la']
+    shell = os.system(comando)        # comando = 'ls -la'
+    print comando
